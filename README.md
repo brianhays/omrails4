bd4337a91536ce03bd2fbb487076ad2d7818afba
Brian Hays <brian.hays@gmail.com>
2013-09-24 11:43:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+omrails4
+==
+
+This is a pinterest style app based on the One Month Rails tutorial by Mattan Griffel - http://onemonthrails.com
+
+While the tutorial currently uses Rails 3, this app is using Rails 4
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails4::Application.config.secret_key_base = 'a60324721d9962caffd7291050b86f47a899f73460958514f3f9da560bf89c038dc2147c9fc4ddc5d1ab87b87e21153e019189a6bc8cf4a49e37bf109f28ea00'
